518c4f88f3a951e32ba29e95c3f715e696cca9f0
guilhermerfs00 <guilherme.rfs00@gmail.com>
2019-02-26 13:41:22 -0300

    Recuperando as MetaTags do tt e Opengraph

--- src/main/java/com/projeto/demoprojeto/domain/SocialMetaTag.java
+++ src/main/java/com/projeto/demoprojeto/domain/SocialMetaTag.java
@@ -1,5 +1,44 @@
 package com.projeto.demoprojeto.domain;
 
-public class SocialMetaTag {
+import java.io.Serializable;
 
+public class SocialMetaTag implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private String title;
+	private String site;
+	private String url;
+	private String image;
+	
+	
+	public String getSite() {
+		return site;
+	}
+	public void setSite(String site) {
+		this.site = site;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	@Override
+	public String toString() {
+		return "SocialMetaTag [title=" + title + ", site=" + site + ", url=" + url + ", image=" + image + "]";
+	}
+	
+	
 }
