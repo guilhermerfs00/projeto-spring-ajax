dab4a07e7ce3cc337dc203517df8dc69d36202da
guilhermerfs00 <guilherme.rfs00@gmail.com>
2019-02-27 11:02:59 -0300

    Carregando com retorno do formulário e retorno de erro

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -1,22 +1,40 @@
-//FUNÇÃO PARA CAPTURAR AS META TAGS
-$("#linkPromocao").on('change', function(){
+//funcao para capturar as meta tags
+$("#linkPromocao").on('change', function() {
+
 	var url = $(this).val();
 	
-	if(url.length > 7){
+	if (url.length > 7) {
+		
 		$.ajax({
-			method: "POST",
+			method:"POST",
 			url: "/meta/info?url=" + url,
-			cache: false, 
-			success: function(data){
+			cache: false,
+			beforeSend: function() {
+				$("#alert").removeClass("alert alert-danger").text('');
+				$("#site").text("");
+				$("#titulo").val("");
+	
+				$("#linkImagem").attr("src", "");
+				$("#loader-img").addClass("loader");
+			},
+			success: function( data ) {
 				console.log(data);
-				$("#titulo").val(data.titulo);
-				$("#site").text(data.site.replace("@" , ""));
+				$("#linkTitle").val(data.title);
+				$("#site").text(data.site.replace("@", ""));
 				$("#linkImagem").attr("src", data.image);
 			},
 			statusCode: {
-				404: function(){
-					$("#alert").addClass("alert alert-danger").text("Nenhuma informação foi recuperado dessa Url.");
+				404: function() {
+					$("#alert").addClass("alert alert-danger").text("Nenhuma informação pode ser recuperada dessa url.");
+					$("#linkImagem").attr("src", "/images/promo-dark.png");
 				}
+			},
+			error: function() {
+				$("#alert").addClass("alert alert-danger").text("Ops... algo deu errado, tente mais tarde.");
+				$("#linkImagem").attr("src", "/images/promo-dark.png");
+			},
+			complete: function() {
+				$("#loader-img").removeClass("loader");
 			}
 		});
 	}
