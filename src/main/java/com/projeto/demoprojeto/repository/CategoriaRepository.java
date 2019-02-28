664b725c251a22a234ab2100b51b38a817089c96
guilhermerfs00 <guilherme.rfs00@gmail.com>
2019-02-28 08:22:46 -0300

    Criando o Repository

---
+++ src/main/java/com/projeto/demoprojeto/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.demoprojeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.demoprojeto.domain.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
---
+++ src/main/java/com/projeto/demoprojeto/repository/PromocaoRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.demoprojeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.demoprojeto.domain.Promocoes;
+
+public interface PromocaoRepository extends JpaRepository<Promocoes, Long>{
+
+}
