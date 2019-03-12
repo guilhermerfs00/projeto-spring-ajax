d7e50c0d4a37df68d5b0af47232d1aca2f4d0771
guilhermerfs00 <guilherme.rfs00@gmail.com>
2019-03-12 10:37:50 -0300

    Metodo post para o Ajax

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -57,8 +57,21 @@ function loadByScrollBar(pageNumber) {
 	})  
 }
 
-//Adicionar os linkes
+//adicionar likes
 $(document).on("click", "button[id*='likes-btn-']", function() {
 	var id = $(this).attr("id").split("-")[2];
 	console.log("id: ", id);
+	
+	$.ajax({
+		method: "POST",
+		url: "/promocao/like/" + id,
+		success: function(response) {
+			$("#likes-count-" + id).text(response);
+		},
+		error: function(xhr) {
+			alert("Ops, ocorreu um erro: " + xhr.status + ", " + xhr.statusText);
+		}
+	});
 });
+
+
