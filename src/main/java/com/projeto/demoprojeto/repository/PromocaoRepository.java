fad79ec9a24021f8ef7ce6e35168c3c571db0b0c
guilhermerfs00 <guilherme.rfs00@gmail.com>
2019-03-12 13:52:22 -0300

    Botaão de like funcionando

--- src/main/java/com/projeto/demoprojeto/repository/PromocaoRepository.java
+++ src/main/java/com/projeto/demoprojeto/repository/PromocaoRepository.java
@@ -12,9 +12,9 @@ public interface PromocaoRepository extends JpaRepository<Promocoes, Long>{
 	
 	@Transactional(readOnly = false)
 	@Modifying
-	@Query("update Promocao p set p.likes = p.likes + 1 where p.id = :id")
+	@Query("update Promocoes p set p.likes = p.likes + 1 where p.id = :id")
 	void updateSomarLikes(@Param("id") Long id);
 	
-	@Query("select p.likes from Promocao p where p.id = :id")
+	@Query("select p.likes from Promocoes p where p.id = :id")
 	int findLikesById(@Param("id") Long id);
 }
--- src/main/java/com/projeto/demoprojeto/web/controller/PromocaoController.java
+++ src/main/java/com/projeto/demoprojeto/web/controller/PromocaoController.java
@@ -17,6 +17,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,6 +38,14 @@ public class PromocaoController {
     private PromocaoRepository promocaoRepository;
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@PostMapping("/like/{id}")
+	public ResponseEntity<?> adicionarLikes(@PathVariable("id") Long id) {
+		promocaoRepository.updateSomarLikes(id);
+		int likes = promocaoRepository.findLikesById(id);
+		return ResponseEntity.ok(likes);
+	}
+	
 	
 	@PostMapping("/save")
 	public ResponseEntity<?> salvarPromocao(@Valid Promocoes promocao, BindingResult result) {
